ce83ababe54979be7fe9921b6cc2b3bc42738ff6
Creusel <victor.creusel@gmail.com>
2017-10-19 11:19:03 +0200

    Game launches

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,9 +1,44 @@
 package model;
 
-public class Main {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-	public static void main(String[] args){
-		//TODO
+public class Main extends BasicGame{
+
+	public Main(String title) {
+		super(title);
 	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public static void main(String[] args){
+		try {
+			new AppGameContainer(new Main("title"), 640, 480, false).start();
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 
 }
