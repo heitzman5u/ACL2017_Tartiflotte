1cb4588a4db5b3fa5cfcfa40177d22ad9b936cb3
Creusel <victor.creusel@gmail.com>
2017-10-19 12:22:33 +0200

    fixed a bug that broke the app when no input is given while pressing enter

--- src/model/Game.java
+++ src/model/Game.java
@@ -26,20 +26,22 @@ public class Game {
 	public void update(){
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		char carac = str.charAt(0);
-		if (carac == 'z' || carac == 'Z'){
-			hero.move(0, -1);
+		if(str.length() > 0){
+			char carac = str.charAt(0);
+			if (carac == 'z' || carac == 'Z'){
+				hero.move(0, -1);
+			}
+			if (carac == 'q' || carac == 'Q'){
+				hero.move(-1, 0);
+			}
+			if (carac == 's' || carac == 'S'){
+				hero.move(0, 1);
+			}
+			if (carac == 'd' || carac == 'D'){
+				hero.move(1, 0);
+			}
 		}
-		if (carac == 'q' || carac == 'Q'){
-			hero.move(-1, 0);
-		}
-		if (carac == 's' || carac == 'S'){
-			hero.move(0, 1);
-		}
-		if (carac == 'd' || carac == 'D'){
-			hero.move(1, 0);
-		}
-
+		
 	}
 	
 	public void render(){
