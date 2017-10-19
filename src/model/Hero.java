05488f137579dfda1fc196081e19121b47ef1b19
dalessandro <vive-les-palmiersx2@live.fr>
2017-10-19 11:49:45 +0200

    new fonctions for class Hero

--- src/model/Hero.java
+++ src/model/Hero.java
@@ -1,9 +1,31 @@
 package model;
 
+import org.newdawn.slick.geom.Vector2f;
+
 public class Hero {
-	private float x;
-	private float y;
+	private Vector2f pos;
 	
+	public Hero(){
+		pos = new Vector2f(0,0);
+	}
 	
+	public void move(float x, float y){
+		pos.add(new Vector2f(x,y));
+	}
+	
+	public Vector2f getPos(){
+		return pos;
+	}
+	
+	public void render(){
+		System.out.println("[ " + pos.getX() + ", " + pos.getY() + " ]\n");
+	}
+	
+	public static void main(String[] args){
+		Hero h = new Hero();
+		h.render();
+		h.move(2.0f, 3.0f);
+		h.render();
+	}
 
 }
