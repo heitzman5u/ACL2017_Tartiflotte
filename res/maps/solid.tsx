e7fa347bd2ad56331e268aeb609f5c4505d420e2
heitzman5u <maureen.heitzmann@gmail.com>
2017-10-31 14:53:58 +0100

    layer solid to detect collisions

---
+++ res/maps/solid.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="solid" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="images/solid.png" width="160" height="160"/>
+</tileset>
