faaa5db6165eed6ea130968d80b231d81b6f3c2c
heitzman5u <maureen.heitzmann@gmail.com>
2017-11-02 18:09:46 +0100

    modifications of the monster's sprite + monster move when the hero is next to him

--- src/model/Monster.java
+++ src/model/Monster.java
@@ -11,8 +11,15 @@ public class Monster extends Character {
 	
 	private Animation[] animations;
 	
+	private boolean moving;
+	private int direction;
+	
+	
 	public Monster(float x, float y){
-		super(x, y, 0.2f);
+		super(x, y, 2.0f);
+		
+		moving = false;
+		direction = 0;
 		
 		animations = new Animation[8];
 		try {
@@ -23,19 +30,64 @@ public class Monster extends Character {
 	}
 
 	public void move(int delta){
-		//TODO
+		float xHero = world.distanceWithHero(this).x;
+		float yHero = world.distanceWithHero(this).y;
+		
+		if(((Math.pow(Math.abs(xHero), 2.0) + Math.pow(Math.abs(yHero), 2.0)) <= 20000) && ((Math.pow(Math.abs(xHero), 2.0) + Math.pow(Math.abs(yHero), 2.0)) >= 600)){
+			moving = true;
+			pos.add(direction(xHero, yHero).scale(speed));	
+		} else {
+			moving = false;
+		}
+	}
+	
+	private Vector2f direction(float xHero, float yHero){
+		int signX, signY;
+		if (xHero <= 0){
+			signX = -1; 
+			if (Math.abs(yHero) <= 3 && Math.abs(yHero) >= 0){
+				direction = 1;
+			}
+		} else {
+			signX = 1;
+			if (Math.abs(yHero) <= 3 && Math.abs(yHero) >= 0){
+				direction = 2;
+			}
+		}
+		
+		if (yHero <= 0){
+			signY = -1;
+			if (!(Math.abs(yHero) <= 3 && Math.abs(yHero) >= 0)){
+				direction = 3;
+			}
+		} else {
+			signY = 1;
+			if (!(Math.abs(yHero) <= 3 && Math.abs(yHero) >= 0)){
+				direction = 0;
+			}
+		}
+		 return new Vector2f(signX, signY);
 	}
 	
 	public void update(int delta){
-		world.distanceWithHero(this);
 		move(delta);
 	}
 	
 	public void render(Graphics g){
 		g.setColor(new Color(48,48,48));
 		g.fillOval(pos.x-20, pos.y, 40, 16);
-		g.drawAnimation(animations[4], pos.x-40, pos.y-65);
+		g.drawAnimation(animations[direction + (moving ? 4 : 0)], pos.x-40, pos.y-65);
 	}
+	
+//	private void selectAnimation(){
+//		if (moving == false){
+//			
+//		}
+//		if (direction.y == 1){
+//			
+//		}
+//		
+//	}
 	
 	private void creationAnimations() throws SlickException{
 		SpriteSheet spriteSheet = new SpriteSheet("lycan", getClass().getResourceAsStream("/monsters/images/lycan.png"), 80, 80);
@@ -44,7 +96,7 @@ public class Monster extends Character {
 		int nbDirections = 4;
 		for (int i = 0 ; i < nbDirections ; i++){
 			Animation animation = new Animation();
-			animation.addFrame(spriteSheet.getSprite(0, i), 100);
+			animation.addFrame(spriteSheet.getSprite(0, i), 200);
 			animations[i] = animation;
 		}
 		
@@ -52,7 +104,7 @@ public class Monster extends Character {
 		for (int j = 0 ; j < 4 ; j++){
 			Animation animation = new Animation();
 			for (int i = 0 ; i < 4 ; i++){
-				animation.addFrame(spriteSheet.getSprite(i, j), 100);
+				animation.addFrame(spriteSheet.getSprite(i, j), 200);
 			}
 			animations[j+nbDirections] = animation;
 		}
