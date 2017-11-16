40fa77758e8e428b759d45b82ab8f01e1c3e3a2d
heitzman5u <maureen.heitzmann@gmail.com>
2017-11-16 16:05:58 +0100

    class World Object

---
+++ src/model/WorldObject.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class WorldObject {
+	protected World world;
+	protected Vector2f pos;
+	
+	protected WorldObject(float x, float y){
+		pos = new Vector2f(x, y);
+	}
+
+	public Vector2f getPos(){
+		return pos;
+	}
+	
+	public float getX(){
+		return pos.x;
+	}
+	
+	public float getY(){
+		return pos.y;
+	}
+	
+	public void setWorld(World w){
+		world = w;
+	}
+}
