47ca7e615ccd5f1a9fca1d422150eb16c3dc824d
vava-like-palmiers <vava-like-palmiers>
2017-11-16 18:31:14 +0100

    add objects in tmx and detection hero on tmx

--- src/model/Level.java
+++ src/model/Level.java
@@ -1,6 +1,5 @@
 package model;
 
-
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,118 +14,134 @@ import exception.NotImplementedException;
 
 /**
  * Load the elements of the map
+ * 
  * @author Tartiflotte
  */
 public class Level {
-	
+
 	private TiledMap map;
 	private Hero hero;
-	
+
 	private Monster monster;
-	
+
 	private Exit exit;
-	
 
 	private ArrayList<LifeFlask> listFlask;
 
 	/**
 	 * Load a level
-	 * @param file file to load the map from
-	 * @param tilesetLoc path to the tileset location
+	 * 
+	 * @param file
+	 *            file to load the map from
+	 * @param tilesetLoc
+	 *            path to the tileset location
 	 * @throws SlickException
 	 */
-	public Level(InputStream file, String tilesetLoc) throws SlickException{
-		if(file == null || tilesetLoc == null){
+	public Level(InputStream file, String tilesetLoc) throws SlickException {
+		if (file == null || tilesetLoc == null) {
 			throw new NullArgumentException();
 		}
-		
+
 		map = new TiledMap(file, tilesetLoc);
 		monster = new Monster(450, 200);
 		hero = new Hero(30, 260);
 		exit = new Exit(new Point(840, 350), new Point(900, 370));
 
-		listFlask = objectsInLevel("flask");		
+		listFlask = objectsInLevel("flask");
 	}
-	
+
 	/**
 	 * Determines if the given position is on a "logic" block of the map
-	 * @param x abscissa of the character x must be within the map
-	 * @param y ordinate of the character y must be within the map
+	 * 
+	 * @param x
+	 *            abscissa of the character x must be within the map
+	 * @param y
+	 *            ordinate of the character y must be within the map
 	 * @return true if the character collides to a wall ; false if not
 	 */
-	public boolean collides(float x, float y){
-		if(x < 0f || x >= (float)(map.getWidth()*map.getTileWidth()) 
-				|| y < 0f || y >= (float)(map.getHeight()*map.getTileHeight()) ){
+	public boolean collides(float x, float y) {
+		if (x < 0f || x >= (float) (map.getWidth() * map.getTileWidth()) || y < 0f
+				|| y >= (float) (map.getHeight() * map.getTileHeight())) {
 			throw new IllegalArgumentException();
 		}
-		
-		Image tile = this.map.getTileImage( //tile wich corresponds with the hero's position
-                (int) x / this.map.getTileWidth(), 
-                (int) y / this.map.getTileHeight(), 
-                this.map.getLayerIndex("logic"));
-		return tile != null; //null if no "logic" tile found there
+
+		Image tile = this.map.getTileImage( // tile wich corresponds with the hero's position
+				(int) x / this.map.getTileWidth(), (int) y / this.map.getTileHeight(), this.map.getLayerIndex("logic"));
+		return tile != null; // null if no "logic" tile found there
 	}
-	
+
 	/**
-	 * return list of objects in type obj in the level.tmx 
+	 * return list of objects in type obj in the level.tmx
+	 * 
 	 * @param obj
-	 * @throws SlickException 
+	 * @throws SlickException
 	 */
-	public ArrayList<LifeFlask> objectsInLevel(String obj) throws SlickException {
+	private ArrayList<LifeFlask> objectsInLevel(String obj) throws SlickException {
 		Image tile;
 		ArrayList<LifeFlask> listFlask = new ArrayList<LifeFlask>();
-		for(int x=0; x < this.map.getWidth(); x++) {
-			for(int y=0; y < this.map.getHeight(); y++) {
+		for (int x = 0; x < this.map.getWidth(); x++) {
+			for (int y = 0; y < this.map.getHeight(); y++) {
 				tile = this.map.getTileImage(x, y, this.map.getLayerIndex(obj));
-				if(tile != null) {
-					listFlask.add(new LifeFlask(x*this.map.getTileWidth(),y*this.map.getTileHeight()));
+				if (tile != null) {
+					listFlask.add(new LifeFlask(x * this.map.getTileWidth(), y * this.map.getTileHeight()));
 				}
 			}
 		}
 		return listFlask;
 	}
-	
+
+	private Hero getHeroInTmx() throws SlickException {
+		for (int x = 0; x < this.map.getWidth(); x++) {
+			for (int y = 0; y < this.map.getHeight(); y++) {
+				if (this.map.getTileImage(x, y, this.map.getLayerIndex("hero")) != null)
+					return new Hero(x * this.map.getTileWidth(), y * this.map.getTileHeight());
+			}
+		}
+		return null;
+	}
+
 	/**
 	 * @return width in tiles
 	 */
-	public int getWidth(){
+	public int getWidth() {
 		return map.getWidth();
 	}
-	
+
 	/**
 	 * @return height in tiles
 	 */
-	public int getHeight(){
+	public int getHeight() {
 		return map.getHeight();
 	}
-	
-	public TiledMap getMap(){
+
+	public TiledMap getMap() {
 		return map;
 	}
-	
-	public Hero getHero(){
+
+	public Hero getHero() {
 		return hero;
 	}
-	
-	public Monster getMonster(){
+
+	public Monster getMonster() {
 		return monster;
 	}
-	
-	public Exit getExit(){
+
+	public Exit getExit() {
 		return exit;
 	}
-	
-	public Iterator<LifeFlask> getFlasks(){
+
+	public Iterator<LifeFlask> getFlasks() {
 		return listFlask.iterator();
 	}
 
 	/**
 	 * Access level's monsters
+	 * 
 	 * @deprecated Not implemented yet
 	 * @return null
 	 */
 	@Deprecated
-	public Iterator<Monster> getMonsters(){
+	public Iterator<Monster> getMonsters() {
 		throw new NotImplementedException();
 	}
 }
