762e3dd1307b0275eb599663bd36a3f42a680abb
Maureen <mo.mo88@hotmail.fr>
2017-11-24 14:40:47 +0100

    little modification hud victory

--- src/model/HudMessage.java
+++ src/model/HudMessage.java
@@ -1,8 +1,13 @@
 package model;
 
+import java.awt.Font;
+
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.Color;
 
 import exception.NullArgumentException;
 import exception.TartiException;
@@ -15,7 +20,8 @@ import exception.TartiException;
 public class HudMessage {
 	
 	private Image img;
-
+	
+	private TrueTypeFont ttf;
 	/**
 	 * 
 	 * @param path path to the image file
@@ -25,10 +31,14 @@ public class HudMessage {
 	public HudMessage(String path) throws SlickException, TartiException {
 		if(path == null) throw new NullArgumentException();
 		img = new Image(getClass().getResourceAsStream(path), "victory_achieved", false, 0);
+		
+		Font font = new Font("Time New Roman", Font.PLAIN, 20);
+		ttf = new TrueTypeFont(font, true);
 	}
 	
 	public void render(Graphics g){
 		g.drawImage(img,1120/2 - img.getWidth()/2, 800/2 -img. getHeight()/2);
+		ttf.drawString(500.0f, 470.0f, "Press p", Color.white);
 	}
 
 }
