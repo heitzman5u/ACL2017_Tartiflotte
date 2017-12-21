2d5d22f5b22b33647d1cb88ed43d2a060fc3ee10
vava-like-palmiers <vava-like-palmiers>
2017-12-21 15:59:32 +0100

    add life bar to the boss

--- src/model/Boss.java
+++ src/model/Boss.java
@@ -1,10 +1,13 @@
 package model;
 
+import java.awt.Font;
+
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.geom.Vector2f;
 
 import exception.InvalidArgumentException;
@@ -14,9 +17,14 @@ import exception.TartiException;
 import graphic.GraphicsFactory;
 
 public class Boss extends Monster{
+	
+	private TrueTypeFont ttf;
 
 	public Boss(float x, float y) {
 		super(x, y, 3.0f, 600f, 80000f, 20, 2, 1000);
+		
+		Font font = new Font("Time New Roman", Font.PLAIN, 20);
+		ttf = new TrueTypeFont(font, true);
 	}
 	
 	public void attackSpell() {
@@ -40,22 +48,28 @@ public class Boss extends Monster{
 		world.addSpell(sp);
 	}
 	
+	private void displayName() {
+		ttf.drawString(1120/2-ttf.getWidth("Galdemiche")/2, 720, "Galdemiche", Color.yellow);
+	}
+	
 	/**
 	 * Display the life bar of the monster
 	 * @throws NotLoadedException 
 	 */
 	private void lifeBarHUD() throws NotLoadedException{
-		Image lifeBarImg = GraphicsFactory.getMonsterLifeBarImage();
-		Image lifeImg = GraphicsFactory.getMonsterLifeImage();
+		Image lifeBarImg = GraphicsFactory.getBossLifeBarImage();
+		Image lifeImg = GraphicsFactory.getBossLifeImage();
 		
+		float bigger = 400;
 		float lifeRatio = life/(float)fullLife;
-		float width = (float)lifeImg.getWidth();
+		float width = (float)lifeImg.getWidth() + bigger;
 		float height = (float)lifeImg.getHeight();
-		float widthRatio = (float)lifeImg.getWidth() * lifeRatio;
+		float widthRatio = ((float)lifeImg.getWidth()+bigger) * lifeRatio;
 
-		lifeBarImg.draw(getX() - (width/2), getY() - 60, 1.0f);
-		lifeImg.draw(getX() - (width/2) +1, getY()+1 - 60, widthRatio, height);
+		lifeBarImg.draw(1120/2-width/2, 750, width, height);
+		lifeImg.draw(1120/2-width/2, 753, widthRatio-4, height-6);
 	}
+	
 	
 	@Override
 	public void update(int delta) throws TartiException {
@@ -73,7 +87,7 @@ public class Boss extends Monster{
 //				// TODO Auto-generated catch block
 //				e.printStackTrace();
 //			}
-		
+	
 	}
 	
 	public void render(Graphics g) throws TartiException {
@@ -87,7 +101,8 @@ public class Boss extends Monster{
 		//put the deplacement fonctions
 		g.drawAnimation(animations[1], pos.x-40, pos.y-65);
 		
-		//lifeBarHUD();
+		lifeBarHUD();
+		displayName();
 	}
 }
 	
